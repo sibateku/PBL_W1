0723ccc719f5b8f1965117fbe9be85bb530baf9e
Akito Sakoh <146631166+sibateku@users.noreply.github.com>
2024-11-27 17:47:04 +0900

    schedule.py編集

--- back/schedule.py
+++ back/schedule.py
@@ -19,7 +19,7 @@ def auth_user(id, pw):
         conn.close()
 
 def create_personal_db(account_id):
-    personal_data = f"sql/{account_id}.db"
+    personal_data = f"sql/userdata/{account_id}.db"
     try:
         conn = sqlite3.connect(personal_data)
         cursor = conn.cursor()
@@ -32,7 +32,7 @@ def create_personal_db(account_id):
                 title    TEXT,
                 budget   INTEGER,
                 spent    INTEGER,
-                category TEXT,
+                category INTEGER
             )
         """)
         conn.commit()
@@ -43,21 +43,21 @@ def create_personal_db(account_id):
         conn.close()
 
 def list_personal_data(account_id):
-    personal_data = f"sql/{account_id}.db"
+    personal_data = f"sql/userdata/{account_id}.db"
     try:
         conn = sqlite3.connect(personal_data)
         cursor = conn.cursor()
-        cursor.execute("SELECT id FROM personal_data")
-        id_all = cursor.fetchall()
-        for id in id_all:
-            print(f"ID: {id[0]}")
+        cursor.execute("SELECT title FROM personal_data")
+        titles = cursor.fetchall()
+        for title in titles:
+            print(f"ID: {title[0]}")
     except Exception as e:
         print(f"Error: {e}")
     finally:
         conn.close()
 
 def add_data(account_id, year, month, day, title, budget, category):
-    personal_data = f"sql/{account_id}.db"
+    personal_data = f"sql/userdata/{account_id}.db"
     try:
         conn = sqlite3.connect(personal_data)
         cursor = conn.cursor()
@@ -72,12 +72,28 @@ def add_data(account_id, year, month, day, title, budget, category):
     finally:
         conn.close()
 
+def delete_data(account_id, data_id):
+    personal_data = f"sql/userdata/{account_id}.db"
+    try:
+        conn = sqlite3.connect(personal_data)
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM personal_data WHERE id = ?", (data_id,))
+        conn.commit()
+        if cursor.rowcount > 0:
+            print(f"Record with ID {data_id} deleted successfully.")
+        else:
+            print(f"No record found with ID {data_id}.")
+    except Exception as e:
+        print(f"Error deleting data: {e}")
+    finally:
+        conn.close()
+
 if __name__ == "__main__":
     input_account_id = "taro"
     input_account_pw = "tar0pas!"
 
     if auth_user(input_account_id, input_account_pw):
         create_personal_db(input_account_id)
-        add_data(input_account_id, 2024, 11, 26, "作成", 100, "001")
-        add_data(input_account_id, 2024, 11, 27, "確認", 200, "001")
+        add_data(input_account_id, 2024, 11, 26, "作成", 100, 1)
+        add_data(input_account_id, 2024, 11, 27, "確認", 200, 1)
         list_personal_data(input_account_id)
